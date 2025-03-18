9e9a09a7a1b9d9e74e34ac130cd61743b73f3c62
Stefan Woutersen <stefanwoutersen@gmail.com>
2025-03-18 13:58:33 +0000

    commiting files

--- file_handling_basics.py
+++ file_handling_basics.py
@@ -18,6 +18,8 @@
 
 # print(file_path)
 
+# new best practice is to use pathlib Path instead of os.path
+
 from pathlib import Path
 
 # Get the current working directory
@@ -43,5 +45,21 @@ else:
 
 # Make a new directory
 
-Path.mkdir("new_directory", )
+new_directory_path = current_dir / "new_directory"
 
+Path.mkdir("new_directory", exist_ok=True)
+
+
+# Opening a file
+opened_file = open(file_path)
+print(opened_file)
+
+with open(file_path) as file:
+    # Do stuff with the file contents
+    print(file.read())
+
+print(file)
+
+print(opened_file)
+
+opened_file.close()
